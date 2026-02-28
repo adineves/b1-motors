c33039053b861cdc7a7e2e9da444a4c656b8ac20
Adler Neves <100231184+adineves@users.noreply.github.com>
2026-02-28 19:56:40 -0300

    Add files via upload

--- assets/js/carros.js
+++ assets/js/carros.js
@@ -10,7 +10,7 @@ const carros = [
     cor: "Preto",
     descricao: "Versão Turbo S completa.",
     imagem: [
-      "https://images.unsplash.com/photo-1614200187524-dc4b892acf16"
+      "https://images.unsplash.com/photo-1593353798398-6024b7444bb6?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ]
   }
 ];
